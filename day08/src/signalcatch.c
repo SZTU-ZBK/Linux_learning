f06763826a4effe183ad5dcabdd7e8ac91455fc0
ZBK <3068373705@qq.com>
2026-01-09 09:03:23 +0800

    signal catch function

---
+++ day08/src/signalcatch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+void sys_error(const char *str)
+{
+	perror(str);
+	exit(1);
+}
+void sig_catch(int signo)
+{
+	printf("catch you!! %d \n",signo);
+	return ;
+}
+int main(int argc,char *argv[])
+{
+	signal(SIGINT,sig_catch);
+	while(1);
+	return 0;
+}
