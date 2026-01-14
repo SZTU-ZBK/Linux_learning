4fe9bf98791b6d756a93779d818b49f50974a25e
ZBK <3068373705@qq.com>
2026-01-14 21:04:11 +0800

    pthread producer_consumer

---
+++ day13/src/cond_produce_consumer.c
@@ -0,0 +1,65 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <error.h>
+#include <stdlib.h>
+#include <string.h>
+struct msg{
+	int num;
+	struct msg *next;
+};
+void err_thread(int ret,char *str)
+{
+	if(ret != 0){
+		fprintf(stderr,"%s:%s\n",str,strerror(ret));
+		pthread_exit(NULL);
+	}
+}
+struct msg *head;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //定义并初始化互斥量
+pthread_cond_t has_data = PTHREAD_COND_INITIALIZER; //定义并初始化互斥量
+void *producer(void *arg)
+{
+	while(1){
+
+		struct msg *mp = malloc(sizeof(struct msg));
+		mp ->num = rand()%1000+1;
+		pthread_mutex_lock(&mutex);
+		mp->next = head;
+		head = mp;
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&has_data);
+		printf("---------producer:%d\n",mp->num);
+		sleep(rand()%3);
+	}
+	return NULL;
+}
+void *consumer(void *arg)
+{
+	struct msg *mp;
+	while(1){
+		pthread_mutex_lock(&mutex);
+		while(head == NULL){
+			pthread_cond_wait(&has_data,&mutex);
+		}
+		mp = head;
+		head = mp->next;
+		pthread_mutex_unlock(&mutex);
+		printf("------consumer:%d\n",mp->num);
+		free(mp);
+		sleep(rand()%3);
+	}
+	return NULL;
+}
+int main(int argc,char *argv[])
+{
+	int ret;
+	pthread_t pid,cid;
+	ret = pthread_create(&pid,NULL,producer,NULL);
+	if(ret!=0)err_thread(ret,"pthread_create producer error");
+	ret = pthread_create(&cid,NULL,consumer,NULL);
+	if(ret!=0)err_thread(ret,"pthread_create consumer error");
+	pthread_join(pid,NULL);
+	pthread_join(cid,NULL);
+	return 0;
+}
